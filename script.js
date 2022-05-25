fe0ca64059f7cb34524f8458f3e9e63199fc7481
maybass <103843894+maybass@users.noreply.github.com>
2022-05-25 19:16:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const box = document.querySelector("#box");
+box.addEventListener("mouseenter", ()=> {
+  box.style.background = "blue";
+})
+
+box.addEventListener("mouseleave", ()=> {
+  box.style.background = "black";
+})
+
 
